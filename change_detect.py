de1d6a0ccb2f23cf211a1c5830e22e048c19f8b4
Tomás Philippart <tomasphilippart@gmail.com>
2023-06-26 12:46:45 +0200

    Adding Google Vision API support
    Not yet tested! Creds don't work

--- change_detect.py
+++ change_detect.py
@@ -162,6 +162,21 @@ def perform_ocr(image):
             max_confidence_text = max(valid_texts, key=len)
             return max_confidence_text.strip()
     
+    elif args.ocr == "google_vision":
+        from google.cloud import vision
+        client = vision.ImageAnnotatorClient()
+
+        response = client.text_detection(image=image)
+        texts = response.text_annotations
+
+        if response.error.message:
+            raise Exception(
+                '{}\nFor more info on error messages, check: '
+                'https://cloud.google.com/apis/design/errors'.format(
+                    response.error.message))
+
+        return texts[0].description
+        
     return ""
 
 
